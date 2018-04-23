53649c1ea0de206dc8c651ae435c1f69add39c33
Patrick <plp248@nyu.edu>
2018-04-23 10:17:35 -0400

    simpler terrrain gen - one that i understand a little

--- ProcGenFinal/Assets/Scripts/TextureGenerator.cs
+++ ProcGenFinal/Assets/Scripts/TextureGenerator.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public static class TextureGenerator{
 
-	public static Texture2D TextureFromColorMap(Color[] colourMap, int width, int height)
+	/*public static Texture2D TextureFromColorMap(Color[] colourMap, int width, int height)
 	{
 		Texture2D texture = new Texture2D(width, height);
 		texture.SetPixels(colourMap);
 		texture.Apply();
-	}
+	}*/
 	
 }
