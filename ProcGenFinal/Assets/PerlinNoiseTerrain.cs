2a114309b6cb669d883d8bb32384e8d87d9a4305
Patrick <plp248@nyu.edu>
2018-04-30 12:03:51 -0400

    updated gen, started working w textures

---
+++ ProcGenFinal/Assets/PerlinNoiseTerrain.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinNoiseTerrain : MonoBehaviour
+{
+
+	public int mDivisions;
+	public float mSize;
+	public float mHeight;
+
+	// Use this for initialization
+	void Start () {
+		CreateTerrain();
+	}
+
+	void CreateTerrain()
+	{
+		int divPlusOne = mDivisions + 1;
+		int vertCount = divPlusOne * divPlusOne;
+		
+		Vector3[] verts = new Vector3[vertCount];
+		Vector2[] uvs = new Vector2[vertCount];
+		int[] tris = new int[mDivisions*mDivisions*6];
+
+		float halfSize = mSize * 0.5f;
+		float divisionSize = mSize / mDivisions;
+		
+		Mesh mesh = new Mesh();
+		GetComponent<MeshFilter>().mesh = mesh;
+
+		int triOffset = 0;
+
+		for (int i = 0; i <= mDivisions; i++)
+		{
+			for (int j = 0; j < mDivisions; j++)
+			{
+
+				float xCoord = (float) j / mDivisions;
+				float yCoord = (float) i / mDivisions;
+				float height = Mathf.PerlinNoise(xCoord*4, yCoord*4) + 0.5f * Mathf.PerlinNoise(xCoord*8, yCoord*8) + 0.25f * Mathf.PerlinNoise(xCoord*16, yCoord*16);
+
+				height *= mHeight;
+				
+				verts[i*divPlusOne+j] = new Vector3(-halfSize + j*divisionSize, height, halfSize-i*divisionSize);
+				uvs[i*divPlusOne+j] = new Vector2((float)j/mDivisions, (float)i/mDivisions);
+
+				if (i<mDivisions && j<mDivisions)
+				{
+					int topLeft = i * divPlusOne + j;
+					int botLeft = (i + 1) * divPlusOne + j;
+
+					tris[triOffset] = topLeft;
+					tris[triOffset + 1] = topLeft + 1;
+					tris[triOffset + 2] = botLeft + 1;
+
+					tris[triOffset + 3] = topLeft;
+					tris[triOffset + 4] = botLeft + 1;
+					tris[triOffset + 5] = botLeft;
+
+					triOffset += 6;
+				}
+				
+			}
+		}
+
+		mesh.vertices = verts;
+		mesh.uv = uvs;
+		mesh.triangles = tris;
+		
+		mesh.RecalculateBounds();
+		mesh.RecalculateNormals();
+	}  
+}
---
+++ ProcGenFinal/Assets/RandomFrame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class RandomFrame : MonoBehaviour
+{
+
+	public VideoPlayer vp;
+
+	void Awake()
+	{
+		vp = GetComponent<VideoPlayer>();
+	}
+
+	void ChooseRandomFrame()
+	{
+		vp.frame = (long)Random.Range(0, vp.frameCount);
+		//vp.frame = 10;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Debug.Log("attempt");
+			ChooseRandomFrame();
+		}
+	}
+}
